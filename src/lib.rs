0b9a59f4a61cd4384b6f80ed58bbd18384d81f64
Tomo Kaneko <richNEET@national.shitposting.agency>
2025-10-12 16:32:16 +0900

    Make struct public

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use uefi_raw::table::system::{ SystemTable };
 use uefi_raw::Handle;
 
 #[repr(C)]
-struct BootInfo {
+pub struct BootInfo {
     img_handle: *mut Handle,
     sys_table:  *mut SystemTable,
     mm_ptr: *mut [u8],
