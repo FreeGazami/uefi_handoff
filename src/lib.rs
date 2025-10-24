e6b3cd213c17c626b2f9e0e044aaacefeb4a2e5f
Tomo Kaneko <richNEET@national.shitposting.agency>
2025-10-25 08:56:05 +0900

    extend handoff struct

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,7 @@ use core::ffi::c_void;
 pub struct BootInfo {
     pub image_handle: *mut c_void,
     pub runtime_services: *mut c_void,
-    pub mm_ptr: *mut c_void,
+    pub mm: *mut c_void,
+    pub mm_len: usize,
+    pub configuration_table: *mut c_void,
 }
