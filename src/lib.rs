d0a20de2522f487e426f0ee33b6e84a4206bc63a
Tomo Kaneko <richNEET@national.shitposting.agency>
2025-10-27 23:10:14 +0900

    Add gop attributes

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,7 @@ pub struct BootInfo {
     pub mm: *mut c_void,
     pub mm_len: usize,
     pub acpi_table: *mut c_void,
+    pub frame_buffer_base: u64,
+    pub frame_buffer_size: usize,
+    pub info: mut GraphicsOutputModeInformation,
 }
